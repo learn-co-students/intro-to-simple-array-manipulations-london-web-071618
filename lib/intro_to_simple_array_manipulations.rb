9e4463bd46f09f0951fc71d33ff4a191c8486504
Ramona M <r1mari@yahoo.com>
2018-07-06 06:37:37 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -55,8 +55,10 @@ end
 
 def using_delete(instructors, no_offense_steven)
   instructors=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-no_offense_steven ="Steven"
-instructors.delete("Steven")
+
+
+no_offense_steven = instructors.delete("Steven")
+
 end
 
 def using_delete_at(famous_robots, deleted_robot)
