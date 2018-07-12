5338efe8848ab3332004329defffc58beb0105f8
Ramona M <r1mari@yahoo.com>
2018-07-12 07:54:12 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,31 +10,47 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
 neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end 
 
+# def using_pop(great_hits_of_the_nineties)
+#   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+#   great_hits_of_the_nineties.pop
+# end 
 def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties=["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-   great_hits_of_the_nineties.pop
-end 
+  great_hits_of_the_nineties.pop
+end
+
+# def pop_with_args(chars_in_game_of_thrones)
+#   chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+#   chars_in_game_of_thrones.pop(2)
+# end
 
 def pop_with_args(chars_in_game_of_thrones)
-  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
   chars_in_game_of_thrones.pop(2)
 end
 
-def using_shift(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
- my_favorite_cities.shift
-end
+# def using_shift(my_favorite_cities)
+#   my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+# my_favorite_cities.shift
+# end
+  
+  def using_shift(my_favorite_cities)
+    my_favorite_cities.shift
+  end 
   
  def shift_with_args(ice_cream_brands) 
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   brands_removed = ice_cream_brands.shift(2)
 end
   
-def using_concat( my_favorite_things, more_favs)
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-more_favs = ["mario kart", "flatiron school"]
-my_favorite_things.concat(more_favs) 
+# def using_concat( my_favorite_things, more_favs)
+# my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+# more_favs = ["mario kart", "flatiron school"]
+# my_favorite_things.concat(more_favs) 
+# end
+
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
 end
+  
 
 def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
   list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
@@ -53,13 +69,15 @@ def using_flatten(private_colleges_in_newyork)
 private_colleges_in_newyork.flatten 
 end
 
+# def using_delete(instructors, no_offense_steven)
+#   instructors=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
+#   no_offense_steven = instructors.delete("Steven")
+
+# end
 def using_delete(instructors, no_offense_steven)
-  instructors=["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-
-
-no_offense_steven = instructors.delete("Steven")
-
+  instructors.delete(no_offense_steven)
 end
+
 
 def using_delete_at(famous_robots, deleted_robot)
   famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
